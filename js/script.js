12fc51e9786fdec852c6a952ebd44ace9eea623c
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-11-13 16:30:33 +0100

    secondo commit

--- js/script.js
+++ js/script.js
@@ -1,22 +1,41 @@
-var chilometri = prompt('Quanti chilometri desideri fare?');
-var anni = prompt('Quanti anni hai?');
-var prezzoAlChilometro = 0.21;
-var minorenni = 18;
-var anziani = 65;
-var sconto1 = 20;
-var sconto2 = 40;
-var prezzoScontanto;
-var prezzo;
-prezzo = (chilometri * prezzoAlChilometro).toFixed(2);
+// Creiamo un finto biglietto del treno con:
+// Nome passeggero
+// Codice treno (numero casuale tra 90000 e 100000 escluso)
+// Numero carrozza (numero casuale tra 1 e 9)
+// Prezzo calcolato
+// Categoria selezionata dall'utente (offerta)
+var nomeCognome= document.getElementById('name');
+var chilometri = document.getElementById('km');
+var anni = document.getElementById('età').value;
+var calcolaBottone = document.getElementById('bottone')
+calcolaBottone.addEventListener("click",
+  function() {
+    var prezzoAlChilometro = 0.21;
+    var minorenni = 18;
+    var anziani = 65;
+    var sconto1 = 20;
+    var sconto2 = 40;
+    var prezzoScontanto;
+    var prezzo;
+    prezzo = (chilometri * prezzoAlChilometro).toFixed(2);
 
-if (!isNaN(anni) && !isNaN(chilometri)) {
-  if (anni < minorenni) {
-    prezzoScontantosconto =  (prezzo - ((prezzo * sconto1) / 100)).toFixed(2);
-    document.getElementById('sconto').innerHTML = prezzoScontantosconto;
-  } else if (anni >= anziani) {
-    prezzoScontantosconto =  (prezzo - ((prezzo * sconto2) / 100)).toFixed(2);
-    document.getElementById('sconto').innerHTML = prezzoScontantosconto;
-  } else {
-    document.getElementById('sconto').innerHTML = prezzo;
+    if (!isNaN(anni) && !isNaN(chilometri)) {
+      if (anni < minorenni) {
+        prezzoScontantosconto =  (prezzo - ((prezzo * sconto1) / 100)).toFixed(2);
+        document.getElementById('sconto').innerHTML = prezzoScontantosconto;
+      } else if (anni >= anziani) {
+        prezzoScontantosconto =  (prezzo - ((prezzo * sconto2) / 100)).toFixed(2);
+        document.getElementById('sconto').innerHTML = prezzoScontantosconto;
+      } else {
+        document.getElementById('sconto').innerHTML = prezzo;
+      }
+    }
+
   }
-}
+)
+
+
+// var element=document.getElementById("saluto");
+// element.addEventListener('click',  function() {
+//   // codice
+//  });
