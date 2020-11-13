5bf91ea4462f455333539ed3e37fd123c41f03e4
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-11-13 17:07:35 +0100

    reset

--- js/script.js
+++ js/script.js
@@ -4,38 +4,40 @@
 // Numero carrozza (numero casuale tra 1 e 9)
 // Prezzo calcolato
 // Categoria selezionata dall'utente (offerta)
-var nomeCognome= document.getElementById('name');
-var chilometri = document.getElementById('km');
-var anni = document.getElementById('età').value;
+
+// var nomeCognome= document.getElementById('name');
+// var chilometri = document.getElementById('km');
+// var anni = document.getElementById('età').value;
+
+//     var prezzoAlChilometro = 0.21;
+//     var minorenni = 18;
+//     var anziani = 65;
+//     var sconto1 = 20;
+//     var sconto2 = 40;
+//     var prezzoScontanto;
+//     var prezzo;
+//     prezzo = (chilometri * prezzoAlChilometro).toFixed(2);
+//
+//     if (!isNaN(anni) && !isNaN(chilometri)) {
+//       if (anni < minorenni) {
+//         prezzoScontantosconto =  (prezzo - ((prezzo * sconto1) / 100)).toFixed(2);
+//         document.getElementById('sconto').innerHTML = prezzoScontantosconto;
+//       } else if (anni >= anziani) {
+//         prezzoScontantosconto =  (prezzo - ((prezzo * sconto2) / 100)).toFixed(2);
+//         document.getElementById('sconto').innerHTML = prezzoScontantosconto;
+//       } else {
+//         document.getElementById('sconto').innerHTML = prezzo;
+//       }
+//     }
+//
+//   }
+// )
 var calcolaBottone = document.getElementById('bottone')
 calcolaBottone.addEventListener("click",
   function() {
-    var prezzoAlChilometro = 0.21;
-    var minorenni = 18;
-    var anziani = 65;
-    var sconto1 = 20;
-    var sconto2 = 40;
-    var prezzoScontanto;
-    var prezzo;
-    prezzo = (chilometri * prezzoAlChilometro).toFixed(2);
-
-    if (!isNaN(anni) && !isNaN(chilometri)) {
-      if (anni < minorenni) {
-        prezzoScontantosconto =  (prezzo - ((prezzo * sconto1) / 100)).toFixed(2);
-        document.getElementById('sconto').innerHTML = prezzoScontantosconto;
-      } else if (anni >= anziani) {
-        prezzoScontantosconto =  (prezzo - ((prezzo * sconto2) / 100)).toFixed(2);
-        document.getElementById('sconto').innerHTML = prezzoScontantosconto;
-      } else {
-        document.getElementById('sconto').innerHTML = prezzo;
-      }
-    }
-
+    var nomeCognome= document.getElementById('name').value;
+    console.log(nomeCognome)
+    var numeroKm = document.getElementById('km').value;
+    console.log(numeroKm)
   }
 )
-
-
-// var element=document.getElementById("saluto");
-// element.addEventListener('click',  function() {
-//   // codice
-//  });
