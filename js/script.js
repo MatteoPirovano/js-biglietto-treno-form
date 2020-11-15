78c17fcbdd55184bc466ff60f2d6843c3bac5029
Matteo Pirovano <Matteo.Pirovano.Job@gmail.com>
2020-11-15 19:24:41 +0100

    Fine

--- js/script.js
+++ js/script.js
@@ -1,48 +1,61 @@
-// Creiamo un finto biglietto del treno con:
-// Nome passeggero
-// Codice treno (numero casuale tra 90000 e 100000 escluso)
-// Numero carrozza (numero casuale tra 1 e 9)
-// Prezzo calcolato
-// Categoria selezionata dall'utente (offerta)
-
-
-//     var prezzoAlChilometro = 0.21;
-//     var minorenni = 18;
-//     var anziani = 65;
-//     var sconto1 = 20;
-//     var sconto2 = 40;
-//     var prezzoScontanto;
-//     var prezzo;
-//     prezzo = (chilometri * prezzoAlChilometro).toFixed(2);
-//
-//     if (!isNaN(anni) && !isNaN(chilometri)) {
-
-
-//
-//   }
-// )
-var calcolaBottone = document.getElementById('bottone')
+var calcolaBottone = document.getElementById('bottone');
 calcolaBottone.addEventListener("click",
   function() {
     var nomeCognome= document.getElementById('name').value;
-    console.log(nomeCognome);
     var chilometri = document.getElementById('km').value;
-    console.log(chilometri);
     var anni = document.getElementById('anni').value;
-    console.log(anni);
     var prezzoAlChilometro = 0.21;
     var prezzo = (chilometri * prezzoAlChilometro).toFixed(2);
-    console.log(prezzo)
     var prezzoScontanto;
-
+    var messaggio = 'Nessuna offerta'
     if (anni ==  "minorenni") {
       prezzoScontanto =  (prezzo - (prezzo * 0.2)).toFixed(2);
-      document.getElementById('sconto').innerHTML = prezzoScontanto;
+      messaggio = "Sconto minorenni"
+      document.getElementById('sconto').innerHTML = prezzoScontanto
     } else if (anni == "senior" ) {
       prezzoScontanto =  (prezzo - (prezzo * 0.4)).toFixed(2);
+      messaggio = 'Sconto senior'
       document.getElementById('sconto').innerHTML = prezzoScontanto;
     } else {
       document.getElementById('sconto').innerHTML = prezzo;
     }
+    var nomeUtente = document.getElementById('name').value;
+    document.getElementById('nome_utente').innerHTML= nomeUtente;
+    document.getElementById('offerta').innerHTML = messaggio;
+
+    var numeroCarrozza = Math.floor(Math.random() * 9 ) + 1;
+    document.getElementById('carrozza').innerHTML = numeroCarrozza;
+
+    var numeroCP = Math.floor(Math.random() * 10000) + 90000;
+    document.getElementById('CP').innerHTML = numeroCP;
+
+    var biglietto = document.getElementById('dati_biglietto');
+    biglietto.classList.remove('hidden');
+    biglietto.classList.add('show');
+
+    var gifHidden = document.getElementById('gif');
+    gifHidden.className = 'hidden';
   }
-)
+);
+
+var calcolaAnnulla = document.getElementById('annulla')
+calcolaAnnulla.addEventListener("click",
+  function() {
+    document.getElementById('name').value =" ";
+    document.getElementById('km').value =" ";
+    document.getElementById('anni').value =" ";
+    document.getElementById('offerta').innerHTML = " ";
+    document.getElementById('sconto').innerHTML = " ";
+    document.getElementById('nome_utente').innerHTML= " ";
+    document.getElementById('offerta').innerHTML = " ";
+    document.getElementById('carrozza').innerHTML = " ";
+    document.getElementById('CP').innerHTML = " ";
+
+    var biglietto = document.getElementById('dati_biglietto');
+    biglietto.classList.remove('show');
+    biglietto.classList.add('hidden');
+
+    var gifHidden = document.getElementById('gif');
+    gifHidden.className = 'show';
+  }
+);
